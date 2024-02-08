12a6f407584e46ce92e472d64bb4f5637c058c11
shaack <shaack@gmail.com>
2024-02-08 16:38:55 +0100

    ...

---
+++ src/ChessboardSounds.js
@@ -0,0 +1,11 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/cm-chessboard-sounds
+ * License: MIT, see file 'LICENSE'
+ */
+
+import {Extension} from "cm-chessboard/src/model/Extension.js"
+
+export class ChessboardSounds extends Extension {
+
+}
